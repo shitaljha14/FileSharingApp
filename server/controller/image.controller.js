47c54fdf6e00f0a29cff748e13c073ad8c1e136e
shitajha14 <shitaljha2312@gmail.com>
2024-08-10 00:24:07 +0530

    my coomiyt

---
+++ server/controller/image.controller.js
@@ -0,0 +1,31 @@
+import File from "../models/file.js";
+import bcrypt from 'bcrypt';
+
+export const uploadImage=async (request,response)=>{
+    const fileObj={
+        path:request.file.path,
+        name:request.file.originalname
+    }
+    try{
+      const file= await File.create(fileObj);
+      response.status(200).json({ path: `http://localhost:8000/file/${file._id}`});
+     
+      console.log("hiii");
+    }
+    catch(error){
+        console.error(error.message);
+        response.status(500).json({error:error.message})
+    }
+ 
+}
+export const downloadImage = async(request,response) =>{
+    try{
+   const file = await  File.findById(request.params.fileId);
+   file.downloadCount++;
+   await file.save();
+   response.download(file.path,file.name);
+    }catch(error){
+       console.error(error.message);
+       return response.status(500).json({error:error.message}); 
+    }
+}
